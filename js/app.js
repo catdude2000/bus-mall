5526dc3077af1b6c3687cf684a99217dfd53984d
Michael Pace <michael.g.pace@gmail.com>
2022-02-03 18:53:12 -0600

    added more colors to chart results, beautified js online

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 console.log('js file is connected');
 
+//Global variables
 const allImages = [];
 let previouslyPickedProducts = [];
 let oldResultsList = document.getElementById('oldResultsList');
@@ -9,35 +10,29 @@ let imageSectionTag = document.getElementById('all_products');
 let rightImgTag = document.getElementById('right_img');
 let centerImgTag = document.getElementById('center_img');
 let leftImgTag = document.getElementById('left_img');
-
 let chartResults = document.getElementById('myChart');
 let resultsList = document.getElementById('resultsList');
-
 let totalClicks = 0;
-
 let leftProductOnPage = null;
 let centerProductOnPage = null;
 let rightProductOnPage = null;
+let oldArray = [];
 
-function updateStorage(){
+function updateStorage() {
   const arrayString = JSON.stringify(allImages);
   localStorage.setItem('allImagesString', arrayString);
 }
-
-function Product(pName, imageSrc){
+function Product(pName, imageSrc) {
   this.pName = pName;
   this.imageSrc = imageSrc;
   this.clicks = 0;
   this.timesShown = 0;
   allImages.push(this);
 }
-
-let oldArray = [];
-
-function getOldResults(){
+function getOldResults() {
   const data = localStorage.getItem('allImagesString');
   const productData = JSON.parse(data);
-  for(let i = 0; i < productData.length; i++){
+  for (let i = 0; i < productData.length; i++) {
     new Product(productData.pName, productData.imageSrc, productData.clicks, productData.timesShown);
     oldArray.push(productData[i]);
   }
@@ -45,11 +40,11 @@ function getOldResults(){
   console.log('oldarray', oldArray);
 }
 
-function handleOldResultsList(){
+function handleOldResultsList() {
 
   let ul = document.getElementById('oldResultsList');
   ul.innerHTML = '';
-  for(let i = 0; i < oldArray.length; i++){
+  for (let i = 0; i < oldArray.length; i++) {
     let current = oldArray[i];
     let li = document.createElement('li');
     li.textContent = current.pName + ' got ' + current.clicks + ' votes and was shown ' + current.timesShown + ' times.';
@@ -58,19 +53,19 @@ function handleOldResultsList(){
   }
 }
 
-const handleClick = function(event){
+const handleClick = function (event) {
   if (event.target.tagName !== 'IMG') {
     return;
   }
   totalClicks++;
 
-  if(event.target.id === 'left_img') {
+  if (event.target.id === 'left_img') {
     leftProductOnPage.clicks++;
   }
-  if(event.target.id === 'center_img') {
+  if (event.target.id === 'center_img') {
     centerProductOnPage.clicks++;
   }
-  if(event.target.id === 'right_img') {
+  if (event.target.id === 'right_img') {
     rightProductOnPage.clicks++;
   }
   leftProductOnPage.timesShown++;
@@ -79,29 +74,29 @@ const handleClick = function(event){
 
   const tempPickedProducts = [];
   let leftImg;
-  do{
+  do {
     leftImg = Math.floor(Math.random() * allImages.length);
-  } while(
+  } while (
     previouslyPickedProducts.includes(allImages[leftImg]) ||
-    tempPickedProducts.includes(allImages[leftImg])
+        tempPickedProducts.includes(allImages[leftImg])
   );
   tempPickedProducts.push(allImages[leftImg]);
 
   let rightImg;
-  do{
+  do {
     rightImg = Math.floor(Math.random() * allImages.length);
-  } while(
+  } while (
     previouslyPickedProducts.includes(allImages[rightImg]) ||
-    tempPickedProducts.includes(allImages[rightImg])
+        tempPickedProducts.includes(allImages[rightImg])
   );
   tempPickedProducts.push(allImages[rightImg]);
 
   let centerImg;
-  do{
+  do {
     centerImg = Math.floor(Math.random() * allImages.length);
-  } while(
+  } while (
     previouslyPickedProducts.includes(allImages[centerImg]) ||
-    tempPickedProducts.includes(allImages[centerImg])
+        tempPickedProducts.includes(allImages[centerImg])
   );
   tempPickedProducts.push(allImages[centerImg]);
 
@@ -118,17 +113,17 @@ const handleClick = function(event){
   previouslyPickedProducts.push(allImages[centerImg]);
   previouslyPickedProducts.push(allImages[rightImg]);
 
-  if(totalClicks === 25){
+  if (totalClicks === 25) {
     updateStorage();
     imageSectionTag.removeEventListener('click', handleClick);
   }
 };
 
-function handleResultsList(){
+function handleResultsList() {
   console.log('results was clicked');
   let ul = document.getElementById('results');
   ul.innerHTML = '';
-  for(let i = 0; i < allImages.length; i++){
+  for (let i = 0; i < allImages.length; i++) {
     let current = allImages[i];
     let li = document.createElement('li');
     li.textContent = current.pName + ' got ' + current.clicks + ' votes and was shown ' + current.timesShown + ' times.';
@@ -136,7 +131,7 @@ function handleResultsList(){
   }
 }
 
-function handleChartResults(){
+function handleChartResults() {
   console.log('chartresultswasclicked');
   makeAProductChart();
 }
@@ -170,21 +165,19 @@ leftProductOnPage = allImages[0];
 centerProductOnPage = allImages[1];
 rightProductOnPage = allImages[2];
 
-function makeAProductChart(){
+function makeAProductChart() {
 
   const productNamesArray = [];
   const productClicksArray = [];
 
-  for(let i = 0; i < allImages.length; i++){
+  for (let i = 0; i < allImages.length; i++) {
     const singleProductName = allImages[i].pName;
     productNamesArray.push(singleProductName);
   }
-
-  for(let i = 0; i < allImages.length; i++){
+  for (let i = 0; i < allImages.length; i++) {
     const singleProductClicks = allImages[i].clicks;
     productClicksArray.push(singleProductClicks);
   }
-
   const ctx = document.getElementById('myChart').getContext('2d');
   // eslint-disable-next-line no-undef
   new Chart(ctx, {
@@ -200,7 +193,20 @@ function makeAProductChart(){
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(200, 100, 64, 0.2)',
+          'rgba(150, 200, 4, 0.2)',
+          'rgba(25, 19, 6, 0.2)',
+          'rgba(285, 150, 60, 0.2)',
+          'rgba(220, 190, 80, 0.2)',
+          'rgba(130, 159, 64, 0.2)',
+          'rgba(250, 100, 50, 0.2)',
+          'rgba(233, 122, 64, 0.2)',
+          'rgba(288, 177, 64, 0.2)',
+          'rgba(90, 180, 80, 0.2)',
+          'rgba(222, 159, 64, 0.2)',
+          'rgba(253, 120, 4, 0.2)',
+          'rgba(214, 100, 94, 0.2)'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
@@ -208,7 +214,20 @@ function makeAProductChart(){
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(255, 159, 64, 1)',
+          'rgba(200, 100, 64, 0.2)',
+          'rgba(150, 200, 4, 0.2)',
+          'rgba(25, 19, 6, 0.2)',
+          'rgba(285, 150, 60, 0.2)',
+          'rgba(220, 190, 80, 0.2)',
+          'rgba(130, 159, 64, 0.2)',
+          'rgba(250, 100, 50, 0.2)',
+          'rgba(233, 122, 64, 0.2)',
+          'rgba(288, 177, 64, 0.2)',
+          'rgba(90, 180, 80, 0.2)',
+          'rgba(222, 159, 64, 0.2)',
+          'rgba(253, 120, 4, 0.2)',
+          'rgba(214, 100, 94, 0.2)'
         ],
         borderWidth: 1
       }]
@@ -224,5 +243,3 @@ function makeAProductChart(){
     }
   });
 }
-
-//how to center images?
